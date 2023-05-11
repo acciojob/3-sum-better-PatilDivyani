cfa2eec7d33f5b459dd5e3e994c74c542d879cb2
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-11 18:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function threeSum(arr, target) {
+function threeSum(nums, target) {
 // write your code here
-	let ans;
-	let check;
-  for (let i = 0; i < arr.length; i++) {
-	  let sum;
-  	for (let j = 0; j < arr.length; j++) {
-  		sum += arr[j];
-		if(sum>ans && sum<check){
-			ans = sum;
-		}
-  	}
-  }
-	return ans;
+	 nums.sort((a, b) => a - b); // sort the input array in ascending order
+    let closestSum = Infinity; // initialize the closest sum to a large number
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+    return closestSum;
 }
 
 module.exports = threeSum;
