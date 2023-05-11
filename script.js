5f62b57b9d5ac6c28434e86ef8e4226b77469714
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-11 18:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let ans;
+	let check;
+  for (let i = 0; i < arr.length; i++) {
+	  let sum;
+  	for (let j = 0; j < arr.length; j++) {
+  		sum += arr[j];
+		if(sum>ans && sum<check){
+			ans = sum;
+		}
+  	}
+  }
+	return ans;
 }
 
 module.exports = threeSum;
